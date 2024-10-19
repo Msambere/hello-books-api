265a80ef5ce85a5d0e5f8e3f91cf6689bd18a024
Amber <110812972+Msambere@users.noreply.github.com>
2024-10-19 12:32:17 -0500

    Create endpoint to read one book and handle errors

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, abort, make_response
 from app.models.book import books
 
 books_bp= Blueprint("books_bp", __name__, url_prefix="/books")
@@ -15,3 +15,27 @@ def get_all_books():
             }
         )
     return response_body
+
+@books_bp.get("/<book_id>")
+def get_one_book(book_id):
+    book = validate_book(book_id)
+    return{
+        "id":book.id,
+        "title": book.title,
+        "description": book.description
+    },200
+
+
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        response = {"msg": f"Book {book_id} is invalid."}
+        abort(make_response(response,400))
+    for book in books:
+        if book.id == book_id:
+            return book
+    response = {"msg": f"Book {book_id} not found."}
+    abort(make_response(response,404))
+
+    
