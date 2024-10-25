d53b534212a41e445029c117a188777fbcc6b369
Amber <110812972+Msambere@users.noreply.github.com>
2024-10-24 22:12:29 -0500

    Change project to work with SQLAlchemy
    Completed lesson 03a, which sets up book model and sets up project to
    work with SQLAlchemy

--- app/models/book.py
+++ app/models/book.py
@@ -1,21 +1,33 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from ..db import db
+
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+    description: Mapped[str]
+
+
+
+
+
 # from uuid import uuid4
 
-class Book:
-    def __init__(self, id, title, description):
-        self.id = id
-        self.title = title
-        self.description = description
+# class Book:
+#     def __init__(self, id, title, description):
+#         self.id = id
+#         self.title = title
+#         self.description = description
 
-    def to_dict(self):
-        return {
-        "id":self.id,
-        "title": self.title,
-        "description": self.description
-    }
+#     def to_dict(self):
+#         return {
+#         "id":self.id,
+#         "title": self.title,
+#         "description": self.description
+#     }
 
-books=[
-    Book(1, "Fictional Book", "A fantasy novel set in an imaginary world."),
-    Book(2, "Wheel of Time", "A fantasy novel set in an imaginary world."),
-    Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
-]
+# books=[
+#     Book(1, "Fictional Book", "A fantasy novel set in an imaginary world."),
+#     Book(2, "Wheel of Time", "A fantasy novel set in an imaginary world."),
+#     Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
+# ]
 
--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,32 +1,32 @@
 from flask import Blueprint, abort, make_response
-from app.models.book import books
+# from app.models.book import books
 
 books_bp= Blueprint("books_bp", __name__, url_prefix="/books")
 
-@books_bp.get("")
-def get_all_books():
-    response_body=[]
-    for book in books:
-        response_body.append(book.to_dict()
-        )
-    return response_body
+# @books_bp.get("")
+# def get_all_books():
+#     response_body=[]
+#     for book in books:
+#         response_body.append(book.to_dict()
+#         )
+#     return response_body
 
-@books_bp.get("/<book_id>")
-def get_one_book(book_id):
-    book = validate_book(book_id)
-    return book.to_dict(), 200
+# @books_bp.get("/<book_id>")
+# def get_one_book(book_id):
+#     book = validate_book(book_id)
+#     return book.to_dict(), 200
 
 
-def validate_book(book_id):
-    try:
-        book_id = int(book_id)
-    except:
-        response = {"msg": f"Book {book_id} is invalid."}
-        abort(make_response(response,400))
-    for book in books:
-        if book.id == book_id:
-            return book
-    response = {"msg": f"Book {book_id} not found."}
-    abort(make_response(response,404))
+# def validate_book(book_id):
+#     try:
+#         book_id = int(book_id)
+#     except:
+#         response = {"msg": f"Book {book_id} is invalid."}
+#         abort(make_response(response,400))
+#     for book in books:
+#         if book.id == book_id:
+#             return book
+#     response = {"msg": f"Book {book_id} not found."}
+#     abort(make_response(response,404))
 
     
