27ddad9859cddf443cf30ec7806b58d7fe415266
Amber <110812972+Msambere@users.noreply.github.com>
2024-10-28 15:17:22 -0500

    Create route and function to delete a book record
    completed lesson 04c

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -50,6 +50,13 @@ def update_book(book_id):
 
     return Response(status=204, mimetype="application/json")
 
+@books_bp.delete("/<book_id>")
+def delete_book(book_id):
+    book=validate_book_id(book_id)
+    db.session.delete(book)
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
 
 
 # Helper Functions
