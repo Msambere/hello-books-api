5a772632bf59646b6c8d8ad1bf78a3319db69e38
Amber <110812972+Msambere@users.noreply.github.com>
2024-10-28 14:56:18 -0500

    Create route and function to get one book
    Complete lesson 04a

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -9,9 +9,7 @@ books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 @books_bp.post("")
 def create_book():
     request_body = request.get_json()
-    title = request_body["title"]
-    description = request_body["description"]
-    validate_new_book_data(title, description)
+    title, description = validate_new_book_data(request_body)
 
     new_book = Book(title=title, description=description)
     db.session.add(new_book)
@@ -38,14 +36,24 @@ def get_all_books():
 
 @books_bp.get("/<book_id>")
 def get_one_book(book_id):
-    book = validate_book(book_id)
+    book = validate_book_id(book_id)
     return book.to_dict(), 200
 
 
 
+# Helper Functions
 
-
-def validate_new_book_data(title, description):
+def validate_new_book_data(request_body):
+    try:
+        title = request_body['title']
+    except:
+        response = {"msg":"Please provide a book title"}
+        abort(make_response(response, 400))
+    try:
+        description = request_body['description']
+    except:
+        response = {"msg":"Please provide a book description"}
+        abort(make_response(response, 400))
     if not isinstance(title, str) or not isinstance(description, str):
         response = {"msg": "Invalid book details"}
         abort(make_response(response, 400))
@@ -57,42 +65,23 @@ def validate_new_book_data(title, description):
     if book_exists:
         response = {"msg": "Book already exists in database."}
         abort(make_response(response, 400))
-        
-    return True
 
-# from app.models.book import books
-
-# @books_bp.get("")
-# def get_all_books():
-#     response_body=[]
-#     for book in books:
-#         response_body.append(book.to_dict()
-#         )
-#     return response_body
-
-# @books_bp.get("/<book_id>")
-# def get_one_book(book_id):
-#     book = validate_book(book_id)
-#     return book.to_dict(), 200
+    return title, description
 
 
-def validate_book(book_id):
+def validate_book_id(book_id):
     try:
         book_id = int(book_id)
     except:
-        response = {"msg": f"Book {book_id} is invalid."}
+        response = {"msg": f"Book id {book_id} is invalid."}
         abort(make_response(response,400))
-
-    # found_book = db.session.query(Book).filter_by(id =book_id).first()
     
-    book_exists = bool(
-        db.session.query(Book).filter_by(id = book_id).first()
-    )
+    query = db.select(Book).where(Book.id == book_id)
+    found_book =db.session.scalar(query)
 
-    if not book_exists:
+
+    if not found_book:
         response = {"msg": f"Book {book_id} not found."}
         abort(make_response(response,404))
     
-    found_book = db.select(Book).where(Book.id == book_id)
-    book =db.session.scalar(found_book) 
-    return book
+    return found_book
