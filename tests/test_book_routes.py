19f186d699a78c0e583e7bf5c710d4422b33bed3
Amber <110812972+Msambere@users.noreply.github.com>
2024-10-30 20:41:45 -0500

    Create more pytests
    Completed lesson 06e

--- tests/test_book_routes.py
+++ tests/test_book_routes.py
@@ -7,7 +7,26 @@ def test_get_all_books_with_no_records(client):
     assert response.status_code == 200
     assert response_body == []
 
-def test_get_one_book(client, two_saved_books):
+def test_get_all_books_with_records(client, two_saved_books):
+    # Act
+    response = client.get("/books")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == [{
+        "id":1,
+        "title":"Ocean Book",
+        "description":"watr 4evr"
+    },
+    {
+        "id":2,
+        "title":"Mountain Book",
+        "description":"i luv 2 climb rocks"
+    }]
+
+
+def test_get_one_book_sucess(client, two_saved_books):
     # Act
     response = client.get("/books/1")
     response_body = response.get_json()
@@ -19,6 +38,25 @@ def test_get_one_book(client, two_saved_books):
         "title":"Ocean Book",
         "description":"watr 4evr"
     }
+
+def test_get_one_book_when_no_records(client):
+    # Act
+    response = client.get("/books/1")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 404
+    assert response_body == {"msg": "Book 1 not found."}
+
+def test_get_one_book_when_id_invalid(client):
+    # Act
+    response = client.get("/books/one")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 400
+    assert response_body == {"msg": "Book id one is invalid."}
+
 
 def test_create_book(client):
     # Act
@@ -35,3 +73,4 @@ def test_create_book(client):
         "title": "New Book",
         "description": "The Best!"
     }
+
