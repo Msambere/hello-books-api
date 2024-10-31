b5e092697e45f67ddf348a439dad66ef37ffaf07
Amber <110812972+Msambere@users.noreply.github.com>
2024-10-30 20:31:01 -0500

    Create test to check post book
    Completed lesson 06e

--- tests/test_book_routes.py
+++ tests/test_book_routes.py
@@ -3,19 +3,35 @@ def test_get_all_books_with_no_records(client):
     response = client.get("/books")
     response_body = response.get_json()
 
-    #Assert
+    # Assert
     assert response.status_code == 200
     assert response_body == []
 
 def test_get_one_book(client, two_saved_books):
-    #Act
+    # Act
     response = client.get("/books/1")
     response_body = response.get_json()
 
-    #Assert
+    # Assert
     assert response.status_code == 200
     assert response_body == {
         "id":1,
         "title":"Ocean Book",
         "description":"watr 4evr"
+    }
+
+def test_create_book(client):
+    # Act
+    response = client.post("/books", json={"title": "New Book", "description": "The Best!"})
+    response_body = response.get_json()
+
+    # Assert
+    # assert response_body["title"] == "New Book"
+    # assert response_body["description"] == "The Best!"
+    # assert response_body["id"]== 1
+    assert response.status_code == 201
+    assert response_body == {
+        "id": 1,
+        "title": "New Book",
+        "description": "The Best!"
     }
